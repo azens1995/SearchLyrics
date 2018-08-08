0252afd98791f0a88c5d36209126e9beb4e8bbd1
azens1995 <azens1995@gmail.com>
2018-08-09 04:24:43 +0545

    Some UI changes

--- app/src/main/java/com/azens/searchlyrics/api/ServiceGenerator.java
+++ app/src/main/java/com/azens/searchlyrics/api/ServiceGenerator.java
@@ -1,6 +1,7 @@
 package com.azens.searchlyrics.api;
 
 import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
